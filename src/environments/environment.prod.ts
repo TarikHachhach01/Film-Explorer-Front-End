302a3270cb1fa815265d0f9186e01e35f2e03f0f
tarik <tari01.hachhach@gmail.comk>
2025-12-02 15:52:38 +0100

    Update services to use environment.apiUrl for Render backend 1

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://film-explorer-backend.onrender.com'  // Production (Render)
+  apiUrl: 'https://film-explorer-backend.onrender.com'  
 };
